af993eaa7143a4d80a4ec2221a82fc1c6598d4ae
FenetGirma <fenutigist@gmail.com>
2024-03-08 17:42:18 +0300

    Functions Exercise 1

---
+++ LAB-1-Functions-ex1.dart
@@ -0,0 +1,11 @@
+double calculateSum(double num1, double num2) {
+  return num1 + num2;
+}
+
+void main() {
+  double firstNumber = 10.5;
+  double secondNumber = 20.3;
+  double sum = calculateSum(firstNumber, secondNumber);
+
+  print("The sum of $firstNumber and $secondNumber is $sum");
+}
