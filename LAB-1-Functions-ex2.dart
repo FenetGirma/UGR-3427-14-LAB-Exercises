971d3986bf6fde8c44c7885ec30f440d0fbb250f
FenetGirma <fenutigist@gmail.com>
2024-03-08 17:43:03 +0300

    Functions Exercise 2

---
+++ LAB-1-Functions-ex2.dart
@@ -0,0 +1,33 @@
+import 'dart:io';
+import 'dart:math';
+
+bool isPrime(int num) {
+  if (num <= 1) {
+    return false;
+  }
+
+  for (int i = 2; i <= sqrt(num); i++) {
+    if (num % i == 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+void main() {
+  print("Enter the upper limit (N) to find prime numbers from 1 to N:");
+  int? N = int.tryParse(stdin.readLineSync()!);
+
+  if (N == null || N < 1) {
+    print("Invalid input. Please enter a positive integer.");
+    return;
+  }
+
+  print("Prime numbers from 1 to $N:");
+  for (int i = 2; i <= N; i++) {
+    if (isPrime(i)) {
+      print(i);
+    }
+  }
+}
