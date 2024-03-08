e3a64a8d70018899898d7d23099785e0615f61d3
FenetGirma <fenutigist@gmail.com>
2024-03-08 17:34:42 +0300

    Variables and Types Exercise 1

---
+++ LAB-1-Variables-and-Types-ex1.dart
@@ -0,0 +1,9 @@
+void main() {
+  String name = 'Fenet Girma';
+  int age = 21;
+  String favoriteColor = 'black';
+
+  print('My name is $name.');
+  print('I am $age years old.');
+  print('My favorite color is $favoriteColor.');
+}
