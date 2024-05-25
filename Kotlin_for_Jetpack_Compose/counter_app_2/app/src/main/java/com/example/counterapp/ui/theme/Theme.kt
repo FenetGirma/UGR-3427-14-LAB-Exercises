5abfd61dbaaa29a6254053be31084c16d1758d33
FenetGirma <fenutigist@gmail.com>
2024-05-25 21:47:48 +0300

    Kotlin for Jetpack Compose

---
+++ Kotlin_for_Jetpack_Compose/counter_app_2/app/src/main/java/com/example/counterapp/ui/theme/Theme.kt
@@ -0,0 +1,36 @@
+package com.example.counterapp.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+private val DarkColorScheme = darkColorScheme(
+    primary = Purple80,
+    secondary = PurpleGrey80,
+    tertiary = Pink80
+)
+
+private val LightColorScheme = lightColorScheme(
+    primary = Purple40,
+    secondary = PurpleGrey40,
+    tertiary = Pink40
+)
+
+@Composable
+fun CounterAppTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colorScheme = if (darkTheme) {
+        DarkColorScheme
+    } else {
+        LightColorScheme
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
+    )
+}
